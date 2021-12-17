efa64737fc7c38d239ea32d014512b8bf30cb76e
Facundo Rodriguez Sabia <facundorsabia@gmail.com>
2021-12-17 14:42:52 -0300

    script

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//Carrousel Swiper
+
+var swiper = new Swiper(".mySwiper", {
+	effect: "flip",
+	grabCursor: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+  });
+
+
 //To Top Button
 
 var btn = $('#button');
@@ -89,19 +104,6 @@ async function handleSubmit(event){
   }
 }
 
-//Carrousel Swiper
-
-var swiper = new Swiper(".mySwiper", {
-	effect: "flip",
-	grabCursor: true,
-	pagination: {
-	  el: ".swiper-pagination",
-	},
-	navigation: {
-	  nextEl: ".swiper-button-next",
-	  prevEl: ".swiper-button-prev",
-	},
-  });
 
 
 //No poner nada aqui debajo, todo debe estar arriba del carrousel ya que existe un error de consola
